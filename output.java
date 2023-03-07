8b991d22d9210a478820b060b901b0fbc4f7a492
Gagandeep Singh <100983473+neophyte0903@users.noreply.github.com>
2023-03-07 14:35:01 +0530

    "revisit"

---
+++ output.java
@@ -0,0 +1,9 @@
+public class output {
+    public static void main(String[] args) {
+        //output
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
